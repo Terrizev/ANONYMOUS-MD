1e43d92617fc28c309c6acfaaa5eaae77cf38848
Anonymous dev <kibuukauthuman0@gmail.com>
2025-05-23 09:58:38 +0300

    Terrizev

--- settings.js
+++ settings.js
@@ -1,3 +1,13 @@
-const _0x59f3a2={};_0x59f3a2.packname=Buffer.from('QW5vbnltb3VzIEJvdA==','base64').toString(),_0x59f3a2.author=Buffer.from('4pq977iP','base64').toString(),_0x59f3a2.botName=Buffer.from('QW5vbnltb3VzIEJvdA==','base64').toString(),_0x59f3a2.botOwner=Buffer.from('VGVycml6ZXY=','base64').toString(),
+const _0x59f3a2 = {};
+_0x59f3a2.packname = Buffer.from('QW5vbnltb3VzIEJvdA==', 'base64').toString(),
+_0x59f3a2.author = Buffer.from('4pq977iP', 'base64').toString(),
+_0x59f3a2.botName = Buffer.from('QW5vbnltb3VzIEJvdA==', 'base64').toString(),
+_0x59f3a2.botOwner = Buffer.from('VGVycml6ZXY=', 'base64').toString(),
 // 🔧 REPLACE BELOW - Add your number and choose any emoji
-_0x59f3a2.ownerNumber='256784670936',_0x59f3a2.giphyApiKey=Buffer.from('cW5sN3NzUUNoVGRQanNLdGEyQXgyTE1hR1h6MzAzdHE=','base64').toString(),_0x59f3a2.commandMode=Buffer.from('cHVibGlj','base64').toString(),_0x59f3a2.description=Buffer.from('VGhpcyBpcyBhIGJvdCBmb3IgbWFuYWdpbmcgZ3JvdXAgY29tbWFuZHMgYW5kIGF1dG9tYXRpbmcgdGFza3Mu','base64').toString(),_0x59f3a2.version=Buffer.from('Mi4wLjA=','base64').toString(),_0x59f3a2.statusReactionEmoji='🍆';module.exports=_0x59f3a2;
+_0x59f3a2.ownerNumber = '256784670936',
+_0x59f3a2.giphyApiKey = Buffer.from('cW5sN3NzUUNoVGRQanNLdGEyQXgyTE1hR1h6MzAzdHE=', 'base64').toString(),
+_0x59f3a2.commandMode = Buffer.from('cHVibGlj', 'base64').toString(),
+_0x59f3a2.description = Buffer.from('VGhpcyBpcyBhIGJvdCBmb3IgbWFuYWdpbmcgZ3JvdXAgY29tbWFuZHMgYW5kIGF1dG9tYXRpbmcgdGFza3Mu', 'base64').toString(),
+_0x59f3a2.version = Buffer.from('Mi4wLjE=', 'base64').toString(), // Changed from Mi4wLjA= (2.0.0) to Mi4wLjE= (2.0.1)
+_0x59f3a2.statusReactionEmoji = '🍆';
+module.exports = _0x59f3a2;
