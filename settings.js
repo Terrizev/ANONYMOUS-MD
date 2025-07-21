d34c6a881a47cfcabbebba889867d23a7fc27a8e
Anonymous dev <kibuukauthuman0@gmail.com>
2025-07-21 18:27:33 +0300

    Terrizev

--- settings.js
+++ settings.js
@@ -2,12 +2,12 @@ const settings = {
   packname: '𝙰𝙽𝙾𝙽𝚈𝙼𝙾𝚄𝚂',
   author: '‎',
   botName: "𝙰𝙽𝙾𝙽𝚈𝙼𝙾𝚄𝚂 𝙱𝙾𝚃",
-  botOwner: '𝐓𝐄𝐑𝐑𝐈𝐙𝐄𝐕', // Your name
-  ownerNumber: '25678670936', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'name', // Your name
+  ownerNumber: '256784670936', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "𝙰𝙳𝚅𝙰𝙽𝙲𝙴𝙳 𝙱𝙾𝚃",
-  version: "4.0.0",
+  version: "4.0.1",
   prefix: "."
 };
 
