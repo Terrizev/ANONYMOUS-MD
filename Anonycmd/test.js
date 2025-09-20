21041ccae1185ca1f842300b811def8656532c83
Anonymous dev <kibuukauthuman0@gmail.com>
2025-09-21 01:24:55 +0300

    Delete Anonycmd directory

--- Anonycmd/test.js
+++
@@ -1,6 +0,0 @@
-let handler = async (m, { reply }) => {
-// pedo
-reply("bug menu is under maintenance, will be replaced with a function in the next update")
-}
-handler.command = ["bugmenu"]
-module.exports = handler
